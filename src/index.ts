58b04c76a840aaa28287f7b11244b847b3b9c0e6
lex62510 <eagle625@tut.by>
2024-04-23 19:31:13 +0300

    add script 6

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,28 @@ const makeRequestToGoogle = () => {
     });
 };
 
-makeRequestToGoogle();
+function getRandomInterval(min:number, max:number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-setInterval(makeRequestToGoogle, 300000);
+function startRandomInterval() {
+    const minInterval = 2 * 60 * 1000; // 2 minutes in milliseconds
+    const maxInterval = 5 * 60 * 1000; // 5 minutes in milliseconds
+
+    const interval = getRandomInterval(minInterval, maxInterval);
+
+    console.log(`Next request will be made in ${interval / 1000} seconds`);
+
+    makeRequestToGoogle(); // Make the initial request
+
+    setInterval(() => {
+        makeRequestToGoogle();
+        const newInterval = getRandomInterval(minInterval, maxInterval);
+        console.log(`Next request will be made in ${newInterval / 1000} seconds`);
+    }, interval);
+}
+
+startRandomInterval();
 
 app.get('/', (req, res) => {
     res.send('Server is ready!');
